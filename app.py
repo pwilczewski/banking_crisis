b87c56928e69b8000a59afd44af4c8aab7f0926f
pwilczewski <pwilczewski@users.noreply.github.com>
2023-04-05 20:43:03 -0500

    added fed balance sheet data

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import gradio as gr
 import pandas as pd
 import numpy as np
-from data_proc import load_equity_data, load_yield_curve_data, load_credit_spread_data
-from data_proc import plot_treasury_curves, plot_credit_spreads
+from data_proc import load_equity_data, load_yield_curve_data, load_credit_spread_data, load_fed_balance_sheet
+from data_proc import plot_treasury_curves, plot_credit_spreads, plot_fed_balance_sheet
 
 loser_frame, largest_frame = load_equity_data()
 yc_frame = load_yield_curve_data()
 spread_frame = load_credit_spread_data()
+fed_frame = load_fed_balance_sheet()
 
 with gr.Blocks() as crisis_dashboard:
     gr.Markdown("<font size=36><center>Banking Crisis Dashboard</center></font>")
@@ -32,7 +33,13 @@ with gr.Blocks() as crisis_dashboard:
                 # gr.Markdown("## <center>Credit spreads</center>")
                 gr.Plot(plot_credit_spreads(spread_frame))
         gr.Markdown("### <center>Data Source: FRED</center>")
-    #with gr.Tab("Fed Balance Sheet"):
+    with gr.Tab("Fed Balance Sheet"):
+        with gr.Row():
+            with gr.Column():
+                gr.Plot(plot_fed_balance_sheet(fed_frame))
+            with gr.Column():
+                gr.Plot(plot_fed_balance_sheet(fed_frame, 'Liabilities'))
+        gr.Markdown("### <center>*change since 3/8/2023")
     #with gr.Tab("Bank Balance Sheets"):
         
 crisis_dashboard.launch()
